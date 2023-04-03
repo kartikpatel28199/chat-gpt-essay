628e2460c9006cfdccaa374cccf4c0d92ba88142
Kartik Patel <kartikpatel281>
2023-04-03 20:01:45 +0530

    Create database connection

--- src/core/config/configuration.ts
+++ src/core/config/configuration.ts
@@ -3,4 +3,11 @@ dotenv.config();
 
 export default {
   port: process.env.PORT,
+  database: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT as any),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    schema: process.env.DB_NAME,
+  },
 };
