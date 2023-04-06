8196daa06fc4ff266e20f1492717a9d540fae4bc
Kartik Patel <kartikpatel281>
2023-04-06 09:17:53 +0530

    Add test endpoint for open-ai

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import ENV from "./core/config/configuration";
 import { AppDataSource } from "./core/database/connection";
+import AppRoutes from "./module/routes";
 
 const app: Express = express();
 const port = ENV.port || 3000;
@@ -16,6 +17,8 @@ AppDataSource.initialize()
   .catch((err) => {
     console.error("Error during Data Source initialization", err);
   });
+
+app.use("", AppRoutes);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
