9bdaa4c11c7575f70adbb5c05dea6bb251b239de
Kartik Patel <kartikpatel281@gmail.com>
2023-05-20 14:29:58 +0530

    Add alertbox when api throws error

--- frontend/src/Interaction/Interaction.tsx
+++ frontend/src/Interaction/Interaction.tsx
@@ -3,16 +3,27 @@ import { FormControl, TextField } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import "./Interaction.css";
 import api from "../common/api";
+import AlertBox from "../common/components/alertbox/alertbox";
 
 function Interaction() {
   const [inputText, setInputText] = useState("");
   const [questionResponse, setQuestionResponse] = useState("Chat gpt response");
   const [isSubmitButtonLoading, setSubmitButtonLoading] = useState(false);
+  const [showAlertBox, setShowAlertBox] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleAskQuestion = async (question: string) => {
     const data = { question };
     const response = await api.openAI.askQuestion(data);
     setSubmitButtonLoading(false);
+    if (response.data.error) {
+      setErrorMessage(response.data.error.message);
+      setShowAlertBox(true);
+      setTimeout(() => {
+        setShowAlertBox(false);
+        setErrorMessage("");
+      }, 3000);
+    }
     setQuestionResponse(response.data.data);
   };
 
@@ -53,6 +64,8 @@ function Interaction() {
           value={questionResponse}
         />
       </div>
+
+      <div>{showAlertBox && <AlertBox message={errorMessage} />}</div>
     </>
   );
 }
