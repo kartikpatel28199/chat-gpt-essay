2400165514040d7003b0f2b69adef4a691f6fe31
Kartik Patel <kartikpatel281@gmail.com>
2023-06-22 09:46:04 +0530

    Add tiptap route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./App.css";
 import Interaction from "./Interaction/Interaction";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Tiptap from "./Tiptap/Tiptap";
 
 function App() {
-  return <div className="App">{<Interaction />}</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Interaction />} />
+          <Route path="tiptap" element={<Tiptap />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/Tiptap/Tiptap.tsx
@@ -0,0 +1,9 @@
+function Tiptap() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default Tiptap;
