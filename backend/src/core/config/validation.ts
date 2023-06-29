dfa1ba702b9318d9394419e81030dd34f50a02bb
Kartik Patel <kartikpatel281@gmail.com>
2023-06-29 09:51:15 +0530

    Remove unwanted environment variables

--- backend/src/core/config/validation.ts
+++ backend/src/core/config/validation.ts
@@ -4,12 +4,6 @@ const validationSchema = Joi.object({
   NODE_ENV: Joi.string().required(),
   PORT: Joi.number().required(),
 
-  DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.required(),
-  DB_USERNAME: Joi.string().required(),
-
-  DB_PASSWORD: Joi.string().required(),
-  DB_NAME: Joi.string().required(),
   DATABASE_URL: Joi.string().required(),
 
   OPENAI_API_KEY: Joi.string().required(),
