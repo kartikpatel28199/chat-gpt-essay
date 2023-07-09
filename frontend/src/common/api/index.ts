3b25cfa380d58851e7969ba7a79dd747ccb9639f
Kartik Patel <kartikpatel281@gmail.com>
2023-07-09 12:31:25 +0530

    AA-21 | Store and fetch the editor content from API

--- frontend/src/common/api/index.ts
+++ frontend/src/common/api/index.ts
@@ -1,6 +1,8 @@
+import editorContentAPI from "./editor-content";
 import openAIAPI from "./openai";
 
 const api = {
   openAI: openAIAPI,
+  editorContent: editorContentAPI,
 };
 export default api;
--- frontend/src/components/Tiptap/index.tsx
+++ frontend/src/components/Tiptap/index.tsx
@@ -5,8 +5,45 @@ import TextStyle from "@tiptap/extension-text-style";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import MenuBar from "./components/Menubar";
+import { useEffect, useState } from "react";
+import api from "../../common/api";
 
 const Tiptap = () => {
+  const [editorContent, setEditorContent] = useState("");
+  const [editorContentId, setEditorContentId] = useState<number>();
+
+  useEffect(() => {
+    if (localStorage.getItem("editorContentId")) {
+      const editorId = localStorage.getItem(
+        "editorContentId"
+      ) as unknown as number;
+      getEditor(editorId);
+    }
+  }, [editorContentId]);
+
+  async function getEditor(editorId: number) {
+    const response = await api.editorContent.getEditorContent(
+      editorId as number
+    );
+    if (!response.data.error) {
+      setEditorContent(response.data.content);
+      setEditorContentId(response.data.id);
+      editor?.commands.setContent(editorContent);
+    }
+  }
+
+  const saveEditorContent = async () => {
+    const data = { content: editorContent, id: editorContentId };
+    const response = await api.editorContent.saveEditorContent(data);
+    if (!response.data.error) {
+      const editorContentId = response.data.id;
+      setEditorContentId(response.data.id);
+      if (!localStorage.getItem("editorContentId")) {
+        localStorage.setItem("editorContentId", editorContentId);
+      }
+    }
+  };
+
   const editor = useEditor({
     extensions: [
       Color.configure({ types: [TextStyle.name, ListItem.name] }),
@@ -14,7 +51,17 @@ const Tiptap = () => {
       StarterKit,
     ],
     onFocus: () => {},
+    onUpdate: ({ editor }) => {
+      setEditorContent(editor.getHTML());
+    },
+    onBlur: () => {
+      saveEditorContent();
+    },
   });
+
+  if (localStorage.getItem("editorContentId") && !editorContentId) {
+    return <>Still loading...</>;
+  }
 
   return (
     <div className="text-editor">
